716ae7daa47b77191fda3bbd9e61da3621f2cd7f
Jayesh Bidani <jayeshbidani@Jayeshs-MacBook-Pro.local>
2024-11-12 21:21:18 +0530

    build

--- src/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React, { useRef } from "react";
-
-const Navbar = ({ navbarRef, toggleNavbar }) => {
-    return <div>Header</div>;
-};
-
-export default Navbar;
--- src/utils/index.jsx
+++
@@ -1,5 +0,0 @@
-const getRandomDegree = (min, max) => {
-    return Math.random() * (max - min) + min;
-};
-
-export { getRandomDegree };
